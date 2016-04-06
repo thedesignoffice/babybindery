8dffc0a68f296682417fc86d644c72703162d229
Lukas <lukaswinklerprins@gmail.com>
2016-04-06 16:34:58 -0400

    initials working.

--- js/label_interview_paragraphs.js
+++ js/label_interview_paragraphs.js
@@ -1,13 +1,12 @@
-  alert('hey');
 
-  var n_paragraphs = $("p").length;
-  var interviewer_initials = $("h1 #interviewer_initials").attr("id");
-  var interviewee_initials = $("h1 #interviewee_initials").attr("id");
-  for(var i=0; i<n_paragraphs; i++){
-    if(i%2==0){
-      $("p").eq(i).prepend("<div class='interviewer initials'>"+interviewer_initials+": </div>");
-    }else{
-      $("p").eq(i).prepend("<div class='interviewee initials'>"+interviewee_initials+": </div>");
-    }
-
+var n_paragraphs = $("p").length;
+var interviewer_initials = $("#interviewer_initials").html();
+var interviewee_initials = $("#interviewee_initials").html();
+for(var i=0; i<n_paragraphs; i++){
+  if(i%2==0){
+    $("p").eq(i).prepend("<span class='interviewer initials'>"+interviewer_initials+": </span>");
+  }else{
+    $("p").eq(i).prepend("<span class='interviewee initials'>"+interviewee_initials+": </span>");
   }
+
+}
