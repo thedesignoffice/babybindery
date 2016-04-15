291f1d3fe3b2a1a8172896e915b1ddc353fc2521
Lukas <lukaswinklerprins@gmail.com>
2016-04-15 09:58:31 -0400

    Interview labeling working well.

--- js/label_interview_paragraphs.js
+++ js/label_interview_paragraphs.js
@@ -1,9 +1,7 @@
 var n_interviews = $(".interview").length;
-alert(n_interviews);
 for(var j=0; j<n_interviews;j++){
   var chosen_interview = $(".interview").eq(j);
   var n_paragraphs = chosen_interview.children("p").length;
-  alert(n_paragraphs);
   var interviewer_initials = chosen_interview.attr("interviewer_initials");
   var interviewee_initials = chosen_interview.attr("interviewee_initials");
   for(var i=0; i<n_paragraphs; i++){
