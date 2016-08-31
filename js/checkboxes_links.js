c515ceeaff95867cde8387e54cc6aeaae9d5476e
Lukas <lukaswinklerprins@gmail.com>
2016-08-30 18:20:23 -0700

    Link index working as best I can tell.

--- js/checkboxes_links.js
+++ js/checkboxes_links.js
@@ -31,6 +31,8 @@ var remove_pages = function(){
 
 function writeLinks(links){
 
+  var link_string = "";
+
   for(var i=0; i<links.length; i++){
 
     var link = links.eq(i);
@@ -47,46 +49,45 @@ function writeLinks(links){
     }
 
     href = temp; // Now stripped & clean=looking.
-
-    //$( "<span class='link_url'>"+href+"</span>" ).insertAfter(link); // This line currently not working. Going the css :after route for now.
-
-    $("#footnotes").append(name+" &rarr; "+href+"<br />");
-
+    link_string = link_string + name+" &rarr; "+href+"<br />";
   }
+  return link_string;
 }
 
 function refreshContent(){
   $(".sheet").css('display','block'); // Reverses anything hidden by remove_pages.
-  $("#footnotes").html("<h1>Links</h1>");
   $("#trim_pages_button").css("text-decoration","none");
   $("#print_button").css("display","none");
+  $("#footnotes").html("<h1>Links</h1>");
 
   var n_checkboxes = $("input[type=checkbox]").length;
   var checked_contents = []; // contains string names of content types whose boxes are checked.
   for(var i=0; i<n_checkboxes;i++){
     var n_checked = checked_contents.length;
     if($('input[type=checkbox]').eq(i).is(':checked')){
-      // Slice off the "checkbox-" prefix in id attribute.
+      // Slice off the "checkbox-" prefix in id attribute and take the content type name that's on the checkbox.
       checked_contents[n_checked] = $('input[type=checkbox]').eq(i).attr('id').slice(9,$('input[type=checkbox]').eq(i).attr('id').length);
     }
   }
   $('.content').css('display','none'); // Clear the slate, then re-flow.
   $('.content').css('break-after','never');
 
+  var link_html = "";
   n_checked = checked_contents.length;
   for(var j=0;j<n_checked;j++){
-    $('.content.'+checked_contents[j]).parents().css('display','block');
+    $('.content.'+checked_contents[j]).parents().css('display','block'); // Display sheets of content types selected.
     $('.content.'+checked_contents[j]).css('display','block');
     $('.content.'+checked_contents[j]).css('break-after','always'); // If this is in all .content blocks, even the hidden .content cause region-breaks. (so we only put it on visible ones)
 
     var links = $('.content.'+checked_contents[j]).find('a');
-    links = links.slice(0,links.length/2); // links in here twice for polyfill reasons.
-    //console.log(links);
-    writeLinks(links);
+
+    link_html = link_html + writeLinks(links);
 
     // This line could be more informative.
     console.log('Flowing in '+checked_contents[j]+' contents.');
   }
+
+  $("#footnotes").html("<h1>Links</h1>"+link_html);
 }
 
 
